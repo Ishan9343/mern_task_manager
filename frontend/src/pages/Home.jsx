22b3331d16230c7ac597c607d2c1a23460e95031
Ishan9343 <139035406+Ishan9343@users.noreply.github.com>
2024-03-15 10:12:35 +0530

    Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
           <div className='bg-purple-100 text-dark h-[50vh] py-20 text-center flex flex-col justify-center items-center '>
              <img className="m-10  w-16 md:w-32 lg:w-48" style={{borderRadius:"200rem",width:"8rem",margin:"2rem"}} src="images/tick.jpg"></img>
            
-            <h1 className="mb-5 text-center sm:text-left" style={{fontFamily:"roboto",fontSize:"4rem"}}><b>Organize Your work, finally!</b></h1>
+            <h1 className="mb-5 text-center sm:text-left" style={{fontFamily:"roboto",fontSize:"3rem"}}><b>Organize Your work, finally!</b></h1>
             <Link to="/signup" className='mt-10 text-xl block space-x-2 hover:space-x-4'>
               <span className='py-2 px-3 cursor-pointer hover:bg-purple-100 transition rounded-sm transition-[margin]'>Start Now</span>
                 
